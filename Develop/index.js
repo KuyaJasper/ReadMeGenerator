458dae48377e7bc83e3b30c9655c7c1b00cbfd11
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-04-14 13:30:33 -0400

    Created Outline for ReadMe

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,64 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown= require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'ProjectTitle',
+        message:'What would you like your project to be called?'
+    },
+    {
+        type: 'input',
+        name: 'Description',
+        message:'Provide a brief description of your project'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message:'Choose a Lincese you would like to use, then press Enter.',
+
+        choices: ["MIT", "I don't want to use a license."]
+    },
+    {
+        type: 'input',
+        name: 'Github',
+        message:'What is your Github user name??'
+    },
+    {
+        type: 'input',
+        name: 'GithubProfile',
+        message:'What is your Github profile link??'
+    },
+    {
+        type: 'input',
+        name: 'Email',
+        message:'What is your Email address?'
+    },
+
+];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>{
+        if (err) throw err;
+    });
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions)
+    .then((answers) => {
+        writeToFile('README.md', generateMarkdown(answers))})
+    // .then((data) => writeToFile('License.md', generateLicense(data)))
+    .then((err) => err ? console.log("There was an error", err) : console.log('Successfully created README!')
+    );
+};
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,85 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "MIT"){
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-red.svg)]`
+  }
+  else {
+    return '';
+  }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+ if (license === "MIT"){
+    return `(https://opensource.org/licenses/MIT)`
+  }
+  else if (license === "I don't want to use a license."){
+    return '';
+  }
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license) {
+    return `## License`;
+  }
+  else {
+    return ''
+  }
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return (
+`# ${data.ProjectTitle}
+
+${renderLicenseBadge(data.license)}${renderLicenseLink(data.license)}
+
+## Desctiption
+${data.Description}
+${data.name}
+${data.date}
+
+## Table of Contents
+  * [Installation](#installation)
+  * Usage
+  * License
+  * Contributing
+  * Tests
+  * Questions
+
+## Installation
+
+## Usage
+
+## Links
+    * Application URL
+    *Github Repository
+
+## Contributions
+
+## Tests
+
+## Questions
+
+    Reach out at:
+  [![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/${data.Github})
+  [![Email]](mailto:${data.Email})
+
+`);
+
+}
+
+function generateLicense(data) {
+  console.log(data);
+  return `# ${data.date}, ${data.name}.
 
 `;
 }
 
-module.exports = generateMarkdown;
+
+module.exports = generateMarkdown
+ 
